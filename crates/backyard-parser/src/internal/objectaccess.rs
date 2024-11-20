00a62069b87028414b753e664f3f09a451ab6479
Alzera Cita <alzeracita23@gmail.com>
2024-11-20 10:11:27 +0700

    fix: objectaccess wrong property parser

--- crates/backyard-parser/src/internal/objectaccess.rs
+++ crates/backyard-parser/src/internal/objectaccess.rs
@@ -8,29 +8,23 @@ use crate::{
   utils::{ match_pattern, Lookup },
 };
 
-use super::identifier::IdentifierParser;
+use super::{ call::CallParser, identifier::IdentifierParser };
 
 #[derive(Debug, Clone)]
 pub struct ObjectAccessParser {}
 
 impl ObjectAccessParser {
   pub fn test(tokens: &Vec<Token>, _: &mut LoopArgument) -> Option<Vec<Vec<Token>>> {
-    if
-      let Some(m) = match_pattern(
-        tokens,
-        [
-          Lookup::Equal(vec![TokenType::ObjectAccess, TokenType::NullsafeObjectAccess]),
-          Lookup::Equal(vec![TokenType::Identifier]),
-        ].to_vec()
-      )
-    {
-      return Some(m);
-    }
     match_pattern(
       tokens,
       [
         Lookup::Equal(
-          vec![TokenType::ObjectAccessBracketOpen, TokenType::NullsafeObjectAccessBracketOpen]
+          vec![
+            TokenType::ObjectAccess,
+            TokenType::NullsafeObjectAccess,
+            TokenType::ObjectAccessBracketOpen,
+            TokenType::NullsafeObjectAccessBracketOpen
+          ]
         ),
       ].to_vec()
     )
@@ -41,35 +35,57 @@ impl ObjectAccessParser {
     matched: Vec<Vec<Token>>,
     args: &mut LoopArgument
   ) -> Result<Box<Node>, ParserError> {
-    match matched.len() {
-      2 => {
-        if let [_, prop] = matched.as_slice() {
-          return Ok(
-            ObjectAccessNode::new(
-              args.last_expr.to_owned().unwrap(),
-              IdentifierParser::from_matched(prop)
-            )
+    if let [access_type] = matched.as_slice() {
+      match
+        guard!(access_type.get(0), {
+          return Err(ParserError::internal("ObjectAccess", args));
+        }).token_type
+      {
+        TokenType::ObjectAccess | TokenType::NullsafeObjectAccess => {
+          let expr = guard!(
+            parser.get_statement(
+              &mut LoopArgument::new(
+                "objectaccess",
+                &[],
+                &[
+                  TokenType::Semicolon,
+                  TokenType::ObjectAccess,
+                  TokenType::NullsafeObjectAccess,
+                  TokenType::ObjectAccessBracketOpen,
+                  TokenType::NullsafeObjectAccessBracketOpen,
+                ],
+                &[
+                  (CallParser::test, CallParser::parse),
+                  (IdentifierParser::test, IdentifierParser::parse),
+                ]
+              )
+            )?,
+            {
+              return Err(ParserError::internal("ObjectAccess", args));
+            }
           );
+          parser.position += 1;
+          return Ok(ObjectAccessNode::new(args.last_expr.to_owned().unwrap(), expr));
         }
-      }
-      1 => {
-        let expr = guard!(
-          parser.get_statement(
-            &mut LoopArgument::with_tokens(
-              "objectaccess",
-              &[TokenType::ObjectAccessBracketClose],
-              &[]
-            )
-          )?,
-          {
-            return Err(ParserError::internal("ObjectAccess", args));
-          }
-        );
-        parser.position += 1;
-        return Ok(ObjectAccessNode::new(args.last_expr.to_owned().unwrap(), expr));
-      }
-      _ => {
-        return Err(ParserError::internal("ObjectAccess", args));
+        TokenType::ObjectAccessBracketOpen | TokenType::NullsafeObjectAccessBracketOpen => {
+          let expr = guard!(
+            parser.get_statement(
+              &mut LoopArgument::with_tokens(
+                "objectaccess",
+                &[TokenType::ObjectAccessBracketClose],
+                &[]
+              )
+            )?,
+            {
+              return Err(ParserError::internal("ObjectAccess", args));
+            }
+          );
+          parser.position += 1;
+          return Ok(ObjectAccessNode::new(args.last_expr.to_owned().unwrap(), expr));
+        }
+        _ => {
+          return Err(ParserError::internal("ObjectAccess", args));
+        }
       }
     }
     Err(ParserError::internal("ObjectAccess", args))
