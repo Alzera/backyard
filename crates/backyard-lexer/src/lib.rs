4217336df27dd40cc81441eba69f07a12cb67069
Alzera Cita <alzeracita23@gmail.com>
2024-12-18 23:53:00 +0700

    fix: constraining dependencies

--- crates/backyard-lexer/src/lib.rs
+++ crates/backyard-lexer/src/lib.rs
@@ -9,6 +9,22 @@ use error::LexError;
 use lexer::Lexer;
 use token::Token;
 
+pub fn byte_lex<'a>(is_eval: bool, input: &[u8]) -> Result<Vec<Token>, LexError> {
+  let arena = Bump::new();
+  let result = arena_byte_lex(&arena, is_eval, input)?;
+  Ok(Vec::from_iter(result.into_iter()))
+}
+
+pub fn arena_byte_lex<'a>(
+  arena: &'a Bump,
+  is_eval: bool,
+  input: &[u8]
+) -> Result<bumpalo::collections::Vec<'a, Token>, LexError> {
+  let mut lexer = Lexer::new(arena, BString::new(input.to_vec()));
+  lexer.start(is_eval)?;
+  Ok(lexer.tokens)
+}
+
 pub fn lex<'a>(is_eval: bool, input: &str) -> Result<Vec<Token>, LexError> {
   let arena = Bump::new();
   let result = arena_lex(&arena, is_eval, input)?;
